ed6d960a491877eba2a0bb7940161c63fe791ed9
corsacca <corsacca@gmail.com>
2021-02-16 14:36:32 +0100

    Make Plugin Update Checker available on multisite when the active theme is not Disciple.Tools

--- disciple-tools-metrics-export.php
+++ disciple-tools-metrics-export.php
@@ -107,18 +107,6 @@ class DT_Metrics_Export {
             }
         }
 
-        if ( is_admin() ) {
-            // Check for plugin updates
-            if ( ! class_exists( 'Puc_v4_Factory' ) ) {
-                require( get_template_directory() . '/dt-core/libraries/plugin-update-checker/plugin-update-checker.php' );
-            }
-            $hosted_json = "https://raw.githubusercontent.com/DiscipleTools/disciple-tools-metrics-export/master/version-control.json";
-            Puc_v4_Factory::buildUpdateChecker(
-                $hosted_json,
-                __FILE__,
-                'disciple-tools-metrics-export'
-            );
-        }
     }
 
     public function register_post_type() {
@@ -253,4 +241,32 @@ if ( !function_exists( "dt_hook_ajax_notice_handler" )){
 }
 
 
+/**
+ * Check for plugin updates even when the active theme is not Disciple.Tools
+ *
+ * Below is the publicly hosted .json file that carries the version information. This file can be hosted
+ * anywhere as long as it is publicly accessible. You can download the version file listed below and use it as
+ * a template.
+ * Also, see the instructions for version updating to understand the steps involved.
+ * @see https://github.com/DiscipleTools/disciple-tools-version-control/wiki/How-to-Update-the-Starter-Plugin
+ */
+add_action( 'plugins_loaded', function (){
+    if ( is_admin() ){
+        // Check for plugin updates
+        if ( ! class_exists( 'Puc_v4_Factory' ) ) {
+            if ( file_exists( get_template_directory() . '/dt-core/libraries/plugin-update-checker/plugin-update-checker.php' )){
+                require( get_template_directory() . '/dt-core/libraries/plugin-update-checker/plugin-update-checker.php' );
+            }
+        }
+        if ( class_exists( 'Puc_v4_Factory' ) ){
+            $hosted_json = "https://raw.githubusercontent.com/DiscipleTools/disciple-tools-metrics-export/master/version-control.json";
+            Puc_v4_Factory::buildUpdateChecker(
+                $hosted_json,
+                __FILE__,
+                'disciple-tools-metrics-export'
+            );
+
+        }
+    }
+} );
 
